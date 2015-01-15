4602905314162909e25b2ff004570aa9b74d8898
juanifioren <juanifioren@gmail.com>
2015-01-15 16:29:19 -0300

    Delete client creation stuff. (should not be in the package)

--- openid_provider/urls.py
+++ openid_provider/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.views.decorators.csrf import csrf_exempt
+from openid_provider.views import *
 
-from openid_provider.views import clients
-from openid_provider.views import endpoints
 
 urlpatterns = patterns('',
 
-	url(r'^authorize/$', endpoints.AuthorizeView.as_view(), name='authorize'),
-	url(r'^token/$', csrf_exempt(endpoints.TokenView.as_view()), name='token'),
-	url(r'^userinfo/$', csrf_exempt(endpoints.userinfo), name='userinfo'),
-
-	url(r'^login/$', 'django.contrib.auth.views.login', { 'template_name': 'openid_provider/login.html' }, name='login'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout', { 'template_name': 'openid_provider/logout.html' }, name='logout'),
-
-	url(r'^clients/$', clients.ClientListView.as_view(), name='client_list'),
-	url(r'^clients/(?P<pk>[\d]+)/$', clients.ClientDetailView.as_view(), name='client_detail'),
-	url(r'^clients/create/$', clients.client_create, name='client_create'),
+	url(r'^authorize/$', AuthorizeView.as_view(), name='authorize'),
+	url(r'^token/$', csrf_exempt(TokenView.as_view()), name='token'),
+	url(r'^userinfo/$', csrf_exempt(userinfo), name='userinfo'),
 
 )
