d5749739d652ca57e7d3004dca137adb508f8685
juanifioren <juanifioren@gmail.com>
2015-04-28 14:55:35 -0300

    Update example project.

--- example_project/provider_app/settings.py
+++ example_project/provider_app/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'provider_app',
     'oidc_provider',
 )
 
--- example_project/provider_app/urls.py
+++ example_project/provider_app/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import views as auth_views
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
+	url(r'^accounts/login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 
     url(r'^admin/', include(admin.site.urls)),
