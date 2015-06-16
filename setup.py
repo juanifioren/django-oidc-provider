0a0db229977e7170dc15cec92ffb8e8199e0b584
juanifioren <juanifioren@gmail.com>
2015-06-16 16:10:39 -0300

    Bump version 0.0.6.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-oidc-provider',
-    version='0.0.5',
+    version='0.0.6',
     packages=[
         'oidc_provider', 'oidc_provider/lib', 'oidc_provider/lib/endpoints',
         'oidc_provider/lib/utils', 'oidc_provider/tests', 'oidc_provider/migrations',
