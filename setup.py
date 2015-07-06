8dc8c31396fed537a4b2fbdb156bbbf121c3b51d
Juan Ignacio Fiorentino <juanifioren@gmail.com>
2015-07-06 13:03:51 -0300

    Update setup.

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
