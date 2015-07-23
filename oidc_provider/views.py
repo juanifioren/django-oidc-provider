895ca36ab57e868268a6f67d76e2cc096fd9879a
juanifioren <juanifioren@gmail.com>
2015-07-23 16:17:28 -0300

    Add decode "utf-8" to jwks_uri view. My fault!

--- oidc_provider/views.py
+++ oidc_provider/views.py
@@ -167,8 +167,8 @@ class JwksView(View):
             'alg': 'RS256',
             'use': 'sig',
             'kid': md5(key).hexdigest(),
-            'n': long_to_base64(public_key.n).encode('utf-8'),
-            'e': long_to_base64(public_key.e).encode('utf-8'),
+            'n': long_to_base64(public_key.n).decode('utf-8'),
+            'e': long_to_base64(public_key.e).decode('utf-8'),
         })
 
         return JsonResponse(dic)
