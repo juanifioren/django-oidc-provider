7658257d1874b8f868c0f12fcac7178e3e5cb343
juanifioren <juanifioren@gmail.com>
2015-07-31 14:19:53 -0300

    Refactoring in discovery.

--- oidc_provider/views.py
+++ oidc_provider/views.py
@@ -2,6 +2,7 @@ import logging
 
 from Crypto.PublicKey import RSA
 from django.contrib.auth.views import redirect_to_login, logout
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
@@ -11,11 +12,10 @@ from hashlib import md5
 from jwkest import long_to_base64
 
 from oidc_provider.lib.endpoints.authorize import *
-from oidc_provider.lib.endpoints.discovery import *
 from oidc_provider.lib.endpoints.token import *
 from oidc_provider.lib.endpoints.userinfo import *
 from oidc_provider.lib.errors import *
-from oidc_provider.lib.utils.common import get_rsa_key
+from oidc_provider.lib.utils.common import get_issuer, get_rsa_key
 
 
 logger = logging.getLogger(__name__)
@@ -148,8 +148,27 @@ def userinfo(request):
 class ProviderInfoView(View):
 
     def get(self, request, *args, **kwargs):
+        dic = dict()
 
-        dic = ProviderInfoEndpoint.create_response_dic()
+        dic['issuer'] = get_issuer()
+
+        SITE_URL = settings.get('SITE_URL')
+
+        dic['authorization_endpoint'] = SITE_URL + reverse('oidc_provider:authorize')
+        dic['token_endpoint'] = SITE_URL + reverse('oidc_provider:token')
+        dic['userinfo_endpoint'] = SITE_URL + reverse('oidc_provider:userinfo')
+        dic['end_session_endpoint'] = SITE_URL + reverse('oidc_provider:logout')
+
+        from oidc_provider.models import Client
+        types_supported = [x[0] for x in Client.RESPONSE_TYPE_CHOICES]
+        dic['response_types_supported'] = types_supported
+
+        dic['jwks_uri'] = SITE_URL + reverse('oidc_provider:jwks')
+
+        dic['id_token_signing_alg_values_supported'] = ['RS256']
+
+        # See: http://openid.net/specs/openid-connect-core-1_0.html#SubjectIDTypes
+        dic['subject_types_supported'] = ['public']
 
         return JsonResponse(dic)
 
@@ -175,6 +194,7 @@ class JwksView(View):
 
 
 class LogoutView(View):
+
     def get(self, request, *args, **kwargs):
         # We should actually verify if the requested redirect URI is safe
         return logout(request, next_page=request.GET.get('post_logout_redirect_uri'))
