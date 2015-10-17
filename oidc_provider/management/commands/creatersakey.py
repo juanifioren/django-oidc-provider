daee582207d13e0f210376222e0fa2e740425467
Pablo SEMINARIO <pablo@seminar.io>
2015-10-17 23:02:37 +0200

    Remove unused CommandError import

--- oidc_provider/management/commands/creatersakey.py
+++ oidc_provider/management/commands/creatersakey.py
@@ -1,7 +1,7 @@
 from Crypto.PublicKey import RSA
 
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
