2573a600920bbd3b3b492972539855cf722c0b84
Ignacio Fiorentino <juanifioren@gmail.com>
2016-08-04 13:35:27 -0300

    Fix test app urls and templates loaders in settings.

--- oidc_provider/tests/app/urls.py
+++ oidc_provider/tests/app/urls.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import views as auth_views
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, {'template_name': 'accounts/logout.html'}, name='logout'),
@@ -12,4 +12,4 @@ urlpatterns = patterns('',
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
