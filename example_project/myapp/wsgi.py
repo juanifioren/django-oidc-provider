55c53b4bc7edf9643659cf976f47038605da04b7
Ignacio Fiorentino <juanifioren@gmail.com>
2016-09-07 16:02:31 -0300

    Change templates of example project.

--- example_project/myapp/wsgi.py
+++ example_project/myapp/wsgi.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "provider_app.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
