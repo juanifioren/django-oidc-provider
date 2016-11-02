41556382264ea2ef560bef0c22df7499a0fc8d5f
Ignacio Fiorentino <juanifioren@gmail.com>
2016-11-02 12:28:39 -0300

    Bump version v0.4.3.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-oidc-provider',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
