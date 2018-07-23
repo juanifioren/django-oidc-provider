eb183c7f0ee054bc42e534ceb1755317cbcabb5c
Dan Collins <dcollinsn@gmail.com>
2018-07-23 13:01:58 -0400

    Add support for Django 2.1, drop support for Django < 1.11
    Django 1.11 deprecated the django.contrib.auth.views.logout
    function-based view, which django-oidc-provider relied on. This
    patchset instead subclasses the new LogoutView.
    
    LogoutView was introduced in Django 1.11. logout() was deprecated in
    1.11 and removed in 2.1. Accordingly, this patch adds Django 2.1 to
    CI and removes 1.8, 1.9, and 1.10.
    
    Resolves #258

--- oidc_provider/tests/app/urls.py
+++ oidc_provider/tests/app/urls.py
@@ -10,9 +10,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^accounts/login/$',
-        auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+        auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     url(r'^accounts/logout/$',
-        auth_views.logout, {'template_name': 'accounts/logout.html'}, name='logout'),
+        auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
     url(r'^admin/', admin.site.urls),
 ]
