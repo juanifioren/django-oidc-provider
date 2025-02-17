9d3fc07b58f164628beb8d52121b0f6f58e2c341
orenzhang <orenzhang@ovinc.cn>
2025-02-17 17:23:10 +0800

    feat(translation): add simplified chinese translation

--- oidc_provider/apps.py
+++ oidc_provider/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class OIDCProviderConfig(AppConfig):
 
     name = 'oidc_provider'
-    verbose_name = u'OpenID Connect Provider'
+    verbose_name = _(u'OpenID Connect Provider')
