35092734c0cd90cac91b26d1fd407d24050239a8
Hernán Chilabert <hchilabert@gmail.com>
2025-11-21 22:15:14 -0300

    Refactor models to use abstract base model for consistency and compatibility with modern Django projects

---
+++ oidc_provider/migrations/0028_alter_ids_to_bigautofield.py
@@ -0,0 +1,55 @@
+# Generated migration to convert id fields to BigAutoField
+
+from django.db import migrations
+from django.db import models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("oidc_provider", "0027_alter_rsakey_options"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="client",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="code",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="responsetype",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="rsakey",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="token",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="userconsent",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+    ]
