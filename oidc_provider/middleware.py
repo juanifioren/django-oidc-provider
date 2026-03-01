6e7185e6793a2ba93c30a4124bd4fb033cf03cbc
Aarni Koskela <akx@iki.fi>
2026-03-01 19:07:58 +0200

    Remove check for MiddlewareMixin
    Minimum Django version = 3.2

--- oidc_provider/middleware.py
+++ oidc_provider/middleware.py
@@ -1,8 +1,4 @@
-try:
-    # https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
-    from django.utils.deprecation import MiddlewareMixin
-except ImportError:
-    MiddlewareMixin = object
+from django.utils.deprecation import MiddlewareMixin
 
 from oidc_provider import settings
 from oidc_provider.lib.utils.common import get_browser_state_or_default
